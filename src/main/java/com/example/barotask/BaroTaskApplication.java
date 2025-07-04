8b9f4c4e5ac58afd8ce220b4a3486a2261d76f8b
queenriwon3 <queenriwon3@gmail.com>
2025-07-05 00:30:10 +0900

    first commit

---
+++ src/main/java/com/example/barotask/BaroTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.barotask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaroTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaroTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/barotask/BaroTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.barotask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaroTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
