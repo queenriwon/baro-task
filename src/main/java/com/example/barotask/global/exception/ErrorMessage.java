bb9e15ad1f5267e47f948f32c3b092d0fb1e48b3
queenriwon3 <queenriwon3@gmail.com>
2025-07-05 03:16:07 +0900

    feat(auth): signup api 작성
    회원가입 API 기능 작성은 하였으나
    Spring Security 적용 전

--- src/main/java/com/example/barotask/global/exception/ErrorMessage.java
+++ src/main/java/com/example/barotask/global/exception/ErrorMessage.java
@@ -5,15 +5,7 @@ import lombok.Getter;
 @Getter
 public enum ErrorMessage {
 
-    EMAIL_REQUIRED("이메일을 입력해주세요."),
-    NICKNAME_REQUIRED("닉네임을 입력해주세요."),
-    PASSWORD_REQUIRED("비밀번호를 입력해주세요."),
-    PASSWORD_CONFIRM_REQUIRED("비밀번호 확인을 입력해주세요."),
-    ROLE_REQUIRED("권한을 입력해주세요."),
-
-    INVALID_EMAIL("유효하지 않은 이메일 형식입니다."),
-    INVALID_NICKNAME("유효하지 않은 닉네임 형식입니다."),
-    INVALID_PASSWORD("유효하지 않은 비밀번호 형식입니다."),
+    DUPLICATE_EMAIL("존재하는 이메일입니다."),
     PASSWORD_MISMATCH("비밀번호와 비밀번호 확인이 일치하지 않습니다."),
     INVALID_ROLE("유효하지 않은 권한입니다."),
 
