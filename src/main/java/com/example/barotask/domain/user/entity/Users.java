cc798bff50903d2184ffb982546efec89011023d
queenriwon3 <queenriwon3@gmail.com>
2025-07-05 01:20:43 +0900

    feat(global): users entity 생성
    users 엔티티 생성

---
+++ src/main/java/com/example/barotask/domain/user/entity/Users.java
@@ -0,0 +1,33 @@
+package com.example.barotask.domain.user.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private String nickname;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole userRole;
+
+    @Builder
+    private Users(Long id, String email, String nickname, String password, UserRole userRole) {
+        this.id = id;
+        this.email = email;
+        this.nickname = nickname;
+        this.password = password;
+        this.userRole = userRole;
+    }
+}
