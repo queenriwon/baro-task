0fa829007602dc880b8adb698340f44cfaaa6ff0
queenriwon3 <queenriwon3@gmail.com>
2025-07-05 15:54:58 +0900

    feat(user): ArgumentResolver 구현
    AuthUserDto로 ArgumentResolver 구현

---
+++ src/main/java/com/example/barotask/config/WebConfig.java
@@ -0,0 +1,29 @@
+package com.example.barotask.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final AuthUserArgumentResolver authUserArgumentResolver;
+    private final JwtInterceptor jwtInterceptor;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(authUserArgumentResolver);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(jwtInterceptor)
+                .addPathPatterns("/**");
+    }
+}
+
